313259a87fc995b12a7cffdc29ab3abf1786599d
Luis David Rodriguez Arias <luis.1701721583@ucaldas.edu.co>
2025-01-24 14:28:40 -0500

    Configura Firebase con variable de entorno

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -1,9 +1,17 @@
-from decouple import config
+import os
+import json
 from firebase_admin import credentials, initialize_app
 
-# Inicializa Firebase usando la ruta especificada en el .env
+# Cargar las credenciales desde la variable de entorno
 def init_firebase():
-    firebase_key_path = config("FIREBASE_KEY_PATH")  # Lee la variable del archivo .env
-    cred = credentials.Certificate(firebase_key_path)
+    firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
+    if not firebase_credentials:
+        raise ValueError("La variable de entorno FIREBASE_CREDENTIALS no está configurada.")
+    
+    # Convierte la cadena JSON a un diccionario
+    cred_dict = json.loads(firebase_credentials)
+    cred = credentials.Certificate(cred_dict)
+    
+    # Inicializa Firebase
     initialize_app(cred)
     print("Firebase inicializado correctamente")
